6135bbf83e00dd8fe4587d2df7eeec72a8e16ab1
Rohan Singh <rohansinghwow@gmail.com>
2023-06-10 13:12:19 +0530

    add davinci options

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -21,6 +21,7 @@ export const modelOptions: ModelOptions[] = [
   'gpt-3.5-turbo',
   'gpt-4',
   'gpt-4-32k',
+  'text-davinci-003',
   // 'gpt-3.5-turbo-0301',
   // 'gpt-4-0314',
   // 'gpt-4-32k-0314',
